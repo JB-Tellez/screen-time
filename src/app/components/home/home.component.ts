19206cfd68f2c85b2eec8c1ee0c3539170e2eec9
JB-Tellez <jb.tellez@gmail.com>
2017-08-01 15:52:15 +0200

    Home component skeleton

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  gotoMovies() {
+    this.router.navigate(['/movies']);
+  }
+
+  gotoKidTime() {
+    this.router.navigate(['/kid-time']);
+  }
+
+  gotoParentSettings() {
+    this.router.navigate(['/parent-settings']);
+  }
+
+}
