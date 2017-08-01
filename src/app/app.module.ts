74f0ed04f256fbfd557dd8050855626822579d84
JB-Tellez <jb.tellez@gmail.com>
2017-08-01 15:54:56 +0200

    new components and Movies service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
+import { MoviesService } from './services/movies.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { HomeComponent } from './components/home/home.component';
+import { KidTimeComponent } from './components/kid-time/kid-time.component';
+import { ParentSettingsComponent } from './components/parent-settings/parent-settings.component';
+import { MovieComponent } from './components/movie/movie.component';
+
+const routes:Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'movies', component: MoviesComponent},
+  {path: 'movie/:id', component: MovieComponent},
+  {path: 'kid-time', component: KidTimeComponent},
+  {path: 'parent-settings', component: ParentSettingsComponent},
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    HomeComponent,
+    KidTimeComponent,
+    ParentSettingsComponent,
+    MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [MoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
