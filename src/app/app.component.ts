d5ddb8746c5c64a8cab630acbd13cb8be9a89b24
JB-Tellez <jb.tellez@gmail.com>
2017-08-03 16:28:40 +0200

    temp data fetching until Authorization implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   constructor(private backend:BackendService, private store:Store<State>) {}
 
   ngOnInit() {
+    // TEMP: will move once Authorization implemented
     this.backend.getAdult(1).subscribe(adult => {
       this.store.dispatch({type:'LOAD_ADULT', payload:adult});
     });
