a0d16910a19fcb9c9ad20b1da8ee58a54578e970
Micaela Linda Villani <villani.micaela@gmail.com>
2017-08-04 00:08:52 +0200

    landing style done

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -3,20 +3,18 @@ import { Router } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { KidsLoginComponent } from './kids-login/kids-login.component';
 
-//disabled --> declare const particlesJS: any;
-
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent implements OnInit {
-
+export class LandingComponent implements OnInit {  
   constructor(private router:Router) { }
 
   ngOnInit() {
-    //disabled --> particlesJS.load('../../../assets/javascripts/particles.js', '../../../assets/particles.json', null); 
+    // particlesJS.load('particles-js', this.particleJSON , null); 
+    
   }
 
   gotoHome() {
