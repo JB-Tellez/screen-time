e2e23a06272cbbcb640b39945159efec29a39d70
JB-Tellez <jb.tellez@gmail.com>
2017-08-04 14:37:22 +0200

    refactor to use clearer method names

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,13 +1,26 @@
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Kid } from "../../../shared/model/kid";
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class BackendService {
 
-  constructor(private http:Http) {}
+  constructor(private http: Http) { }
 
-  getAdult(id) {
-    return this.http.get(`http://localhost:3000/adults/${id}`).map( data => data.json() );
+  // TODO: needed?
+  fetchFamilies() {
+    return this.http.get(`http://localhost:3000/families`).map(data => data.json());
   }
 
+  fetchFamily(id) {
+    return this.http.get(`http://localhost:3000/families/${id}`).map(data => data.json());
+  }
+
+   addKid(kid:Kid) {
+    // return this.http.post(`http://localhost:3000/kids`, kid).map(data => data.json());
+    kid.id = Math.floor(Math.random() * 100);
+    return Observable.of(kid);
+  }
 }
