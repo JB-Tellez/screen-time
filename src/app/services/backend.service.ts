fbd017b4be6cc96d38209da0232e1961ffe12f54
JB-Tellez <jb.tellez@gmail.com>
2017-08-06 14:25:07 +0200

    tweak to use proxy config

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,26 +1,30 @@
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Kid } from "../../../shared/model/kid";
 import 'rxjs/add/observable/of';
+import { Kid } from "../store/model";
 
 @Injectable()
 export class BackendService {
 
   constructor(private http: Http) { }
 
-  // TODO: needed?
   fetchFamilies() {
-    return this.http.get(`http://localhost:3000/families`).map(data => data.json());
+    return this.http.get(`api/families`).map(data => data.json());
+  }
+
+  fetchKids() {
+    return this.http.get(`api/kids`).map(data => data.json());
   }
 
   fetchFamily(id) {
-    return this.http.get(`http://localhost:3000/families/${id}`).map(data => data.json());
+
+    return this.http.get(`api/families/${id}`).map( data => data.json() );
   }
 
    addKid(kid:Kid) {
     // return this.http.post(`http://localhost:3000/kids`, kid).map(data => data.json());
-    kid.id = Math.floor(Math.random() * 100);
+    kid.id = '' + Math.floor(Math.random() * 100);
     return Observable.of(kid);
   }
 }
