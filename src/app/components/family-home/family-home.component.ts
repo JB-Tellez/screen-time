db59f8c4e5cc872f3357540b88545dfcecd86c4f
JB-Tellez <jb.tellez@gmail.com>
2017-08-06 14:24:07 +0200

    add links to dashboard and kids in family

--- src/app/components/family-home/family-home.component.ts
+++ src/app/components/family-home/family-home.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
+import { State, Family, Kid } from "../../store/model";
 
 @Component({
   selector: 'app-family-home',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FamilyHomeComponent implements OnInit {
 
-  constructor() { }
+  family$: Observable<Family>;
+  
+  constructor(private store:Store<State>, private route:ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.family$ = this.store.select('app', 'family');
+  }
+
+  gotoDashboard() {
+    
+    // WARNING: snapshot safe?
+    const id = this.route.snapshot.params['id'];
+
+    // TODO: got to be an easier way to just add /dashboard to current route
+    this.router.navigate([`/family/${id}/dashboard`]);
+  }
+
+  gotoKid(kid:Kid) {
+
+    // WARNING: snapshot safe?
+    const id = this.route.snapshot.params['id'];
+
+    this.router.navigate([`/family/${id}/kid/${kid._id}`]);
   }
 
 }
