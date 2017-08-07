bb2aa8788d1a64062aea0621e3628ab42362691e
JB-Tellez <jb.tellez@gmail.com>
2017-08-07 17:20:40 +0200

    add 7 bedtime dates when creating Kid via admin panel

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -43,8 +43,10 @@ export class AdminComponent implements OnInit {
                           // { _id: string, showId: string, movieId: string, title: string, startTime: Date, endTime: Date };
     const viewing:Viewing = {_id:undefined, showId: undefined, movieId: undefined, title:'Gone with the Wind', startTime: new Date(), endTime: new Date()};
 
-    const kid:Kid = {_id: undefined, name:kidName, password:'pass', family:family, minutesPerWeek:400, viewings:[viewing], bedTimes:[new Date()
-      ]}
+    const kid:Kid = {_id: undefined, name:kidName, password:'pass', family:family, 
+      minutesPerWeek:400, 
+      viewings:[viewing], 
+      bedTimes:[new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()]}
     
     this.store.dispatch(new CreateKidAction(kid));
 
