bdeb285c6c6bad3c6e168cf7635bd56555718405
JB-Tellez <jb.tellez@gmail.com>
2017-08-07 17:00:08 +0200

    show bedTimes and viewings

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
-import { State, Family, Kid } from "../../store/model";
+import { State, Family, Kid, Viewing } from "../../store/model";
 
 @Component({
   selector: 'app-admin',
@@ -40,7 +40,11 @@ export class AdminComponent implements OnInit {
 
     const kidName = 'kid_' + Math.floor(Math.random() * 1000);
 
-    const kid:Kid = {_id: undefined, name:kidName, password:'pass', family:family, minutesPerWeek:400, viewings:[], bedTimes:[]}
+                          // { _id: string, showId: string, movieId: string, title: string, startTime: Date, endTime: Date };
+    const viewing:Viewing = {_id:undefined, showId: undefined, movieId: undefined, title:'Gone with the Wind', startTime: new Date(), endTime: new Date()};
+
+    const kid:Kid = {_id: undefined, name:kidName, password:'pass', family:family, minutesPerWeek:400, viewings:[viewing], bedTimes:[new Date()
+      ]}
     
     this.store.dispatch(new CreateKidAction(kid));
 
@@ -49,6 +53,14 @@ export class AdminComponent implements OnInit {
   kidClicked(family: Family, kid:Kid) {
     console.log('family', family, 'kid', kid);
     this.router.navigate([`/family/${family._id}/kid/${kid._id}`]);
+  }
+
+  viewingClicked(viewing:Viewing) {
+    console.log('viewing', viewing);
+  }
+
+  getTimeSpent(viewing: Viewing) {
+    return +viewing.endTime - +viewing.startTime;
   }
 
 }
