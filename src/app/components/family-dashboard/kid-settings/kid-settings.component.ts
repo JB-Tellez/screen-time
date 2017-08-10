2947ad58d888cedca966134ccf934e042efe9f20
JB-Tellez <jb.tellez@gmail.com>
2017-08-11 01:10:28 +0200

    create new kid

--- src/app/components/family-dashboard/kid-settings/kid-settings.component.ts
+++ src/app/components/family-dashboard/kid-settings/kid-settings.component.ts
@@ -1,6 +1,7 @@
+import { CreateKidAction } from './../../../store/actions';
+import { Store } from '@ngrx/store';
 import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { Kid } from "../../../store/model";
+import { Kid, Family, State } from "../../../store/model";
 
 
 @Component({
@@ -11,12 +12,34 @@ import { Kid } from "../../../store/model";
 export class KidSettingsComponent implements OnInit {
 
   @Input() kid:Kid;
-  
-  myTime1 = new Date();
-  
-  constructor(private router:Router) {}
+  @Input() family:Family;
+
+  avatars = [0,1,2,3];
+
+  constructor(private store:Store<State>) {}
 
   ngOnInit() {
+
+    this.kid = {
+      _id: undefined,
+      name: 'name',
+      avatar: 0,
+      age: 0,
+      password: '',
+      family: undefined,
+      bedTimes: [new Date(),new Date(),new Date(),new Date(),new Date(),new Date(),new Date()],
+      viewings: [],
+      minutesPerWeek: 100,
+      currentViewing: undefined
+    }
+  }
+
+  submitForm(form) {
+    this.kid.family = this.family;
+    this.kid.avatar = ['icon_one','icon_two','icon_three','icon_four'].indexOf(form.value.avatar);
+    console.log('submitForm', this.kid, form);
+    this.store.dispatch(new CreateKidAction(this.kid));
+
   }
 
 }
