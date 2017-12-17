cf005ea6878e0f3ecd62a43f5a67727f32a740a1
Micaela Linda Villani <villani.micaela@gmail.com>
2017-12-17 20:12:12 +0100

    shit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
-  // apiUrl: 'http://screen-time-app.herokuapp.com'
+  // apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://screen-time-server.herokuapp.com'
 };
 
